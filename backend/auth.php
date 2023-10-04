d4d02fe11fc045b5233cfdfa47dba7a1e3447a5a
sergey <ff@ff.ru>
2023-10-04 20:25:24 +0300

    merge docker-compose

--- backend/auth.php
+++ backend/auth.php
@@ -3,6 +3,7 @@ require_once './helpers/client.php';
 
 session_start();
 
+<<<<<<< HEAD
 $request_body = json_decode(file_get_contents('php://input'), true);
 
 $pas = md5($request_body['password'] . 'fsd45%73n');
@@ -12,6 +13,17 @@ if (hasUserWithCredentials($db_connection, $login)) {
   $id = getUserData($db_connection, $login)['id'];
   $user_data = getDashboard($db_connection, $id);
   $dashes = $user_data['dashes'];
+=======
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$pas = md5($_POST['password'] . 'fsd45%73n');
+$login = $_POST['email'];
+
+if (counter($link, $login) != 0) {
+  $id = returnData($link, $login)['id'];
+  $user_data = returnDash($link, $id);
+  $dashes = $user_data['data'];
+>>>>>>> b6ba537 (merge docker-compose)
   $_SESSION['authorised'] = true;
   $_SESSION['name'] = $login;
   $_SESSION['dash'] = $dashes;
